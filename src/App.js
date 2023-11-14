cd81fb886cb2aa2f1a9afded0028f26dcd9da02c
StarlightSSM <suminshin211@gmail.com>
2023-11-14 15:48:04 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class GradeCalculator extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="./style.css" type="text/css" />
+        <title>3_A_201944005_신수민</title>
+        <div id="first">
+          <h2 className="title">1학년</h2>
+          <div className="btn_box">
+            <input type="button" defaultValue="추가" />
+            <input type="button" defaultValue="삭제" />
+            <input type="button" defaultValue="저장" />
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th />
+                <th>이수</th>
+                <th>필수</th>
+                <th>과목명</th>
+                <th>학점</th>
+                <th>출석점수</th>
+                <th>과제점수</th>
+                <th>중간고사</th>
+                <th>기말고사</th>
+                <th>총점</th>
+                <th>평균</th>
+                <th>성적</th>
+              </tr>
+            </thead>
+            <tbody className="scores">
+              {/*과목 별 점수*/}
+            </tbody>
+            <tfoot>
+              <tr className="sum">
+                <td />
+                <td colSpan={3}>합계</td>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div id="second">
+          <h2 className="title">2학년</h2>
+          <div className="btn_box">
+            <input type="button" defaultValue="추가" />
+            <input type="button" defaultValue="삭제" />
+            <input type="button" defaultValue="저장" />
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th />
+                <th>이수</th>
+                <th>필수</th>
+                <th>과목명</th>
+                <th>학점</th>
+                <th>출석점수</th>
+                <th>과제점수</th>
+                <th>중간고사</th>
+                <th>기말고사</th>
+                <th>총점</th>
+                <th>평균</th>
+                <th>성적</th>
+              </tr>
+            </thead>
+            <tbody className="scores">
+              {/*과목 별 점수*/}
+            </tbody>
+            <tfoot>
+              <tr className="sum">
+                <td />
+                <td colSpan={3}>합계</td>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div id="third">
+          <h2 className="title">3학년</h2>
+          <div className="btn_box">
+            <input type="button" defaultValue="추가" />
+            <input type="button" defaultValue="삭제" />
+            <input type="button" defaultValue="저장" />
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th />
+                <th>이수</th>
+                <th>필수</th>
+                <th>과목명</th>
+                <th>학점</th>
+                <th>출석점수</th>
+                <th>과제점수</th>
+                <th>중간고사</th>
+                <th>기말고사</th>
+                <th>총점</th>
+                <th>평균</th>
+                <th>성적</th>
+              </tr>
+            </thead>
+            <tbody className="scores">
+              {/*과목 별 점수*/}
+            </tbody>
+            <tfoot>
+              <tr className="sum">
+                <td />
+                <td colSpan={3}>합계</td>
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default GradeCalculator;
